d4c5251aae0db06c9bc2b1b7a7f0b93fdc6143e5
Mark Delchev <markdelchev@gmail.com>
2023-02-04 22:50:29 +0200

    more optimized code

--- prime_number_finder/main.py
+++ prime_number_finder/main.py
@@ -11,3 +11,22 @@ for i in range(1, number + 1):
 
 else:
     print(f"{number} is a prime number.")
+
+# More optimized version
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for k in range(2, int(n ** 0.5) + 1):
+        if n % k == 0:
+            return False
+    return True
+
+
+number = int(input())
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
+
