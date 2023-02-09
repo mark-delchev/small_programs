b830fd19cf7daa0d75da85bd679ca09e9c4f0f74
Mark Delchev <markdelchev@gmail.com>
2023-02-09 22:14:28 +0200

    list sum finder main file

--- list_sum_finder/list_sum_finder.py
+++ list_sum_finder/list_sum_finder.py
@@ -1,4 +1,4 @@
-class Solution:
+class TwoSum:
     def __init__(self):
         self.lst = []
 
---
+++ list_sum_finder/main.py
@@ -0,0 +1,4 @@
+from list_sum_finder import *
+
+two_sum_obj = TwoSum()
+print(two_sum_obj.two_sum(7, [1, 2, 3, 4]))
