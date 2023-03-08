ea3ae881ceabb76dd523a514ad900cf28704f87e
Mark Delchev <markdelchev@gmail.com>
2023-03-08 20:49:26 +0200

    more realistic spread of temperatures

--- weather/main.py
+++ weather/main.py
@@ -1,7 +1,17 @@
 import random
 import math
 
-degrees_C = random.randint(-32, 45)
-degrees_F = math.floor(degrees_C * 9 / 5 + 32)
+mean = (45 + (-32)) / 1.6
+std_dev = 10
+
+while True:
+    degrees_C = round(random.normalvariate(mean, std_dev))
+    degrees_F = math.floor(degrees_C * 9 / 5 + 32)
+    degrees_K = math.floor(degrees_C + 273.15)
+    if -32 <= degrees_C <= 45:
+        break
+
+
 print(f"{degrees_C} C")
 print(f"{degrees_F} F")
+print(f"{degrees_K} K")
